6326d5f0c8edd3bb5bbb6de8a9c562bede2530b4
1LAE <vermakov@cs.petrsu.ru>
2020-04-26 23:40:38 +0300

    small_updates

--- app/m.c
+++ app/m.c
@@ -22,47 +22,10 @@ void m(char* line, char* num, text txt){
     int n_line = numberise(line);
     int n_num  = numberise(num);
     
-    /*
-    if(n_num > MAXLINE + 1){
-        
-        int flag = 1;
-        
-        fprintf(stderr, YELLOW "NOTE: number is too high. Do you want to plase cursor at the end of the line? (y or n)\n" RESET);
-        
-        
-        while(flag){
-            
-            char* ans;
-            printf(RED);
-            scanf("%s", ans);
-            printf(RESET);
-            if(strlen(ans) != 1){
-                fprintf(stderr, YELLOW "NOTE: please, answer 'y' or 'n'\n" RESET);
-                continue;
-            }
-            
-            switch (ans[0]){
-                case 'y':
-                    n_num = MAXLINE;
-                    flag = 0;
-                    move_crsr(txt, n_line, n_num);
-                    show(txt);
-                    break;
-                case 'n':
-                    fprintf(stderr, YELLOW "NOTE: enter valid cursor position\n" RESET);
-                    flag = 0;
-                    break;
-                default:
-                    fprintf(stderr, YELLOW "NOTE: please, answer 'y' or 'n'\n" RESET);
-                    break;
-            }
-        }
-    } else { 
-        */
+   
         move_crsr(txt, n_line, n_num);
         show(txt);
         
     }
     
 
-//}
