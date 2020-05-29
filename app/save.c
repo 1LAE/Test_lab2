22409ec927a4f05e18291f4e1d20a45fbbe8b3a3
1LAE <60959833+1LAE@users.noreply.github.com>
2020-05-29 11:00:13 +0300

    Update save.c

--- app/save.c
+++ app/save.c
@@ -36,11 +36,11 @@ void save(text txt, char *filename)
 static void save_line(int index, char *contents, int cursor, void *data)
 {
     /* Функция обработчик всегда получает существующую строку */
-    assert(contents != NULL);
+    //assert(contents != NULL);
 
     /* Декларируем неиспользуемые параметры */
-    //UNUSED(index);
-    //UNUSED(cursor);
+    UNUSED(index);
+    UNUSED(cursor);
 
     /* Сохраняем строку в файл */
     FILE* f = (FILE*)data;
